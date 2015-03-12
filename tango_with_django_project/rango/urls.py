8668ca950fe36b1cd22700bb08e23d57c46cea5a
unknown <pankajrathi@pankaj3103.Nagarro.local>
2015-03-12 16:44:44 +0530

    Changes before exercise 7

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, url
 from rango import views
 
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-        url(r'^about/$', views.about, name='about'))
+##urlpatterns = patterns('',
+##        url(r'^$', views.index, name='index'),
+##        url(r'^about/$', views.about, name='about'),
+##        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
 
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category1, name='category1'))  # New!
