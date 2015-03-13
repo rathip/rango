2ae6da5285fad2187ac597353f14f308d3248eef
unknown <pankajrathi@pankaj3103.Nagarro.local>
2015-03-13 14:33:00 +0530

    Chapter 8 before exercise

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -9,4 +9,5 @@ from rango import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category1, name='category1'))  # New!
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 # Import the Category model
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
 ##    context_dict = {'boldmessage': "I am bold font from the context"}
@@ -54,3 +55,28 @@ def category1(request, category_name_slug):
 
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
